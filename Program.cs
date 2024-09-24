39c9c60c2b953736ada5c6d75d5daa2980d1dbcd
nordinzakaria <nordinzakaria@gmail.com>
2024-09-24 15:10:23 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TransactionList transl = new TransactionList();
+
+        while (true)
+        {
+            Console.WriteLine("1) Read 2) Print 3) Quit");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                transl.Read();
+            }
+            else if (option == "2")
+            {
+                transl.Write();
+            }
+            else if (option == "3")
+            {
+                break;
+            }
+        }
+    }
+
+}
---
+++ TransactionList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TransactionList
+{
+    const int TRANSACTION_SZ = 5;
+    Transaction[] dataset = new Transaction[TRANSACTION_SZ];
+
+    public void Read()
+    {
+        Console.WriteLine("Enter " + TRANSACTION_SZ + " transaction values:");
+
+        for (int i = 0; i < TRANSACTION_SZ; i++)
+        {
+            Console.Write("Val: ");
+            string val = Console.ReadLine();
+            dataset[i] = new Transaction();
+
+            dataset[i].data = int.Parse(val);
+
+            dataset[i].date = new Date();
+            Console.Write("Day: ");
+            val = Console.ReadLine();
+            dataset[i].date.day = int.Parse(val);
+
+            Console.Write("Month: ");
+            val = Console.ReadLine();
+            dataset[i].date.month = int.Parse(val);
+
+            Console.Write("Year: ");
+            val = Console.ReadLine();
+            dataset[i].date.year = int.Parse(val);
+        }
+
+    }
+
+    public void Write()
+    {
+        Console.WriteLine("Transaction values:");
+        for (int i = 0; i < TRANSACTION_SZ; i++)
+        {
+            Console.WriteLine("Val: " + dataset[i].data);
+            Console.WriteLine("Day: " + dataset[i].date.day);
+            Console.WriteLine("Month: " + dataset[i].date.month);
+            Console.WriteLine("Year: " + dataset[i].date.year);
+        }
+    }
+
+}
+
